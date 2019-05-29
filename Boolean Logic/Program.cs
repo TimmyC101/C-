8e9211964f555689c52ecb967ee7a4a496d55040
TimmyC101 <tcalhoun101@yahoo.com>
2019-05-29 12:32:37 -0600

    Boolean Logic Drill page 75

---
+++ Boolean Logic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? (Enter true or false)");
+            Boolean dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Boolean qualified = age > 15 && dui == false && tickets <= 3;
+            Console.WriteLine("Qualified? - " + qualified);
+            Console.Read();
+        }
+    }
+}
