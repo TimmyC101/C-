09164d173a2b552f5a727204b77c209c813c4de6
TimmyC101 <tcalhoun101@yahoo.com>
2019-06-03 15:54:26 -0600

    Classes Drill #3 - page 115

---
+++ Classes3/Maths.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes3
+{
+    class Maths
+    {
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Addition(int num1, out int num3, int num2 = 5)
+        {
+            num3 = num2;
+            return num1 + num2;
+        }
+    }
+}
---
+++ Classes3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths math = new Maths();
+            Console.WriteLine("Please enter one or two numbers.  You may leave the second number blank.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            string str2 = Console.ReadLine();
+            if (!string.IsNullOrEmpty(str2))
+            {
+                int num2 = Convert.ToInt32(str2);
+                int sum = math.Addition(num1, num2);
+                Console.WriteLine(num1 + " + " + num2 + " = " + sum);
+            }
+            else
+            {
+                int sum = math.Addition(num1, out int num3);
+                Console.WriteLine(num1 + " + " + num3 + " = " + sum);
+            }
+            Console.ReadLine();
+        }
+    }
+}
