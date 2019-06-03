1e81fde72e5b0e7e8e01905f91b31e3981357a83
TimmyC101 <tcalhoun101@yahoo.com>
2019-06-03 15:05:52 -0600

    Classes drill #2 - page 113

---
+++ Classes2/Maths.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes2
+{
+    class Maths
+    {
+        public void Fxn(int num1, int num2)
+        {
+            int add = num1 + 5;
+            Console.WriteLine(num2);
+            Console.ReadLine();
+        }
+    }
+}
