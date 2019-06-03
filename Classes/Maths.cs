4acf6e0bc75a1335f595b1aa67552f86a39c6ace
TimmyC101 <tcalhoun101@yahoo.com>
2019-06-03 14:26:50 -0600

    Classes drill - page 111

---
+++ Classes/Maths.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Maths
+    {
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths math = new Maths();
+            Console.WriteLine("Please enter two numbers on which to perform teh Maths.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int add = math.Addition(num1, num2);
+            int subtract = math.Subtraction(num1, num2);
+            int multi = math.Multiplication(num1, num2);
+            Console.WriteLine("");
+            Console.WriteLine(num1 + " + " + num2 + " = " + add);
+            Console.WriteLine(num1 + " - " + num2 + " = " + subtract);
+            Console.WriteLine(num1 + " x " + num2 + " = " + multi);
+            Console.ReadLine();
+        }
+    }
+}
