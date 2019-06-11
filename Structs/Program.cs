62452962967cc6e734d88754933195a07d9f370c
TimmyC101 <tcalhoun101@yahoo.com>
2019-06-11 11:06:16 -0600

    Structs

---
+++ Structs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 50.60m;
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
