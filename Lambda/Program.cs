e8213e7962432e29038d0c82c368ba41e7e16f58
TimmyC101 <tcalhoun101@yahoo.com>
2019-06-11 12:35:09 -0600

    Lambda Functions drill - page 140

---
+++ Lambda/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee { first = "Tim", last = "Calhoun", Id = 1 });
+            employees.Add(new Employee { first = "Andy", last = "Calhoun", Id = 2 });
+            employees.Add(new Employee { first = "Matt", last = "Marquez", Id = 3 });
+            employees.Add(new Employee { first = "Joe", last = "Rosenberg", Id = 4 });
+            employees.Add(new Employee { first = "Garyt", last = "Walker", Id = 5 });
+            employees.Add(new Employee { first = "Callen", last = "McLaughlin", Id = 6 });
+            employees.Add(new Employee { first = "Ryan", last = "Chabala", Id = 7 });
+            employees.Add(new Employee { first = "Lauren", last = "Gronberg", Id = 8 });
+            employees.Add(new Employee { first = "Laura", last = "Styles", Id = 9 });
+            employees.Add(new Employee { first = "Joe", last = "Zeusowitz", Id = 10 });
+
+            int count1 = 0;
+            foreach (Employee person in employees)
+            {
+                if (person.first == "Joe")
+                {
+                    count1++;
+                }
+            }
+            Console.WriteLine("Number of Joes using foreach loop: " + count1);
+
+            int count2 = employees.Count(x => x.first == "Joe");
+            Console.WriteLine("Number of Joes using lambda fxn: " + count2);
+
+            int count3 = employees.Count(x => x.Id > 5);
+            Console.WriteLine("Number of employee ID's greater than 5 using lambda fxn: " + count3);
+
+            Console.ReadLine();
+        }
+    }
+}
