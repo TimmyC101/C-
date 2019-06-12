f400eed21e21d15276807870ae3fc6cb828def40
TimmyC101 <tcalhoun101@yahoo.com>
2019-06-12 11:54:20 -0600

    File IO drill - page 154

---
+++ FileIO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number.");
+            string num = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Tim\Documents\Github\C#\FileIO\log.txt", num);
+            string read = File.ReadAllText(@"C:\Users\Tim\Documents\Github\C#\FileIO\log.txt");
+            Console.WriteLine("The number " + read + " has been logged and read back to the user.");
+            Console.ReadLine();
+        }
+    }
+}
