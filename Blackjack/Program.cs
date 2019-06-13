56faa4ecbef100307780aebff5c694684e4759dc
TimmyC101 <tcalhoun101@yahoo.com>
2019-06-13 15:44:53 -0600

    blackjack game updates

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Casino;
 using Casino.Blackjack;
+using System.Data.SqlClient;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +17,20 @@ namespace Blackjack
         {
             Console.WriteLine("Welcome to the Grand Hotel and Casino.  Please tell me your name.");
             string playerName = Console.ReadLine();
-
+            if (playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions();
+                foreach (var exception in Exceptions)
+                {
+                    Console.Write(exception.Id + " | ");
+                    Console.Write(exception.ExceptionType + " | ");
+                    Console.Write(exception.ExceptionMessage + " | ");
+                    Console.Write(exception.TimeStamp + " | ");
+                    Console.WriteLine();
+                }
+                Console.ReadLine();
+                return;
+            }
             bool valid = false;
             int bank = 0;
             while (!valid)
@@ -43,15 +58,17 @@ namespace Blackjack
                     {
                         game.Play();
                     }
-                    catch (FraudException)
+                    catch (FraudException ex)
                     {
-                        Console.WriteLine("Security! Throw this person out!");
+                        Console.WriteLine(ex.Message);
+                        UpdateDbWithException(ex);
                         Console.ReadLine();
                         return;
                     }
-                    catch (Exception)
+                    catch (Exception ex)
                     {
                         Console.WriteLine("An error occurred, please contact your system administrator.");
+                        UpdateDbWithException(ex);
                         Console.ReadLine();
                         return;
                     }
@@ -60,8 +77,62 @@ namespace Blackjack
                 Console.WriteLine("Thank you for playing.");
             }
             Console.WriteLine("Feel free to look around the casino.  Goodbye for now.");
-
             Console.ReadLine();
+        }
+
+        private static void UpdateDbWithException(Exception ex)
+        {
+            string conString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BlackjackGame;Integrated Security=True;
+                                Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;
+                                ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            string queryString = @"INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) VALUES
+                                    (@ExceptionType, @ExceptionMessage, @TimeStamp)";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
+                command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", SqlDbType.DateTime);
+
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        private static List<ExceptionEntity> ReadExceptions()
+        {
+            string conString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BlackjackGame;Integrated Security=True;
+                                Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;
+                                ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            string queryString = @"SELECT Id, ExceptionType, ExceptionMessage, TimeStamp FROM Exceptions";
+
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+            }
+            return Exceptions;
         }
 
     }
